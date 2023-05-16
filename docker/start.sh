c4de98a399b8c8cdcb680838ab2b03e6b5799fd4
Анна Дмитриева <a.dmitrieva@sberauto.com>
2023-05-16 17:13:02 +0300

    DCB-257 | Добавила различие среды для sberTeamId

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,7 @@ find /usr/share/nginx/html -type f -print0 | xargs -0 sed -i "s#DEALERSHIP_REACT
 find /usr/share/nginx/html -type f -print0 | xargs -0 sed -i "s#DEALERSHIP_REACT_APP_API_URL#$DEALERSHIP_REACT_APP_API_URL#g"
 find /usr/share/nginx/html -type f -print0 | xargs -0 sed -i "s#DEALERSHIP_APP_URL#$DEALERSHIP_APP_URL#g"
 find /usr/share/nginx/html -type f -print0 | xargs -0 sed -i "s#DEALERSHIP_ENVIRONMENT#$DEALERSHIP_ENVIRONMENT#g"
+find /usr/share/nginx/html -type f -print0 | xargs -0 sed -i "s#DEALERSHIP_SBER_TEAM_ID_ENVIRONMENT#$DEALERSHIP_SBER_TEAM_ID_ENVIRONMENT#g"
 find /usr/share/nginx/html -type f -print0 | xargs -0 sed -i "s#DEALERSHIP_SBER_TEAM_ID_URL#$DEALERSHIP_SBER_TEAM_ID_URL#g"
 
 nginx -g "daemon off;"
