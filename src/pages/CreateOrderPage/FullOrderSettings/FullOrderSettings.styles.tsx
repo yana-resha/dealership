05c628de1a8906765f3c91c1de35044ed1820f62
Лысков Дмитрий <lyskovdmitrij@m-dlyskov.local>
2023-05-31 09:58:40 +0300

    DCB-380 | Добавлен роут для большого калькулятора

--- src/pages/CreateOrderPage/FullOrderSettings/FullOrderSettings.styles.tsx
+++ src/pages/CreateOrderPage/FullOrderSettings/FullOrderSettings.styles.tsx
@@ -1,6 +1,19 @@
 import { makeStyles } from '@mui/styles'
 
 export const useStyles = makeStyles(theme => ({
+  page: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(6),
+
+    [theme.breakpoints.down('sm')]: {
+      alignItems: 'unset',
+      padding: 0,
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+
   errorContainer: {
     width: '100%',
     padding: theme.spacing(1, 1, 1, 3),
