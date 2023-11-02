254b9728fc943ae5645eae0c3ecd0f304a90d4f7
Дмитрий Лысков <dmitrijlyskov@Admins-MacBook-Pro.local>
2023-11-02 16:35:51 +0300

    DCB-452 | Сделано преобразование значений поля source dв ручке findApplications

--- src/common/findApplication/findApplications/utils/makeApplicationTableData.ts
+++ src/common/findApplication/findApplications/utils/makeApplicationTableData.ts
@@ -3,6 +3,26 @@ import { StatusCode, Application } from '@sberauto/loanapplifecycledc-proto/publ
 import { PreparedTableData } from 'entities/application/ApplicationTable/ApplicationTable.types'
 import { getFullName } from 'shared/utils/clientNameTransform'
 
+const sourceMap = {
+  Sberbankru: 'Сайт',
+  Sberbankru2: 'Сайт',
+  Portalda: 'PortalDA',
+  Sberbankrucall: 'ЕРКЦ',
+  Marketing: 'RBC',
+  broker_before_chat: 'СберАвто',
+  broker_after_chat: 'СберАвто',
+  sbol_after_chat: 'СберАвто',
+  autoteka: 'Автотека',
+  autoru: 'Auto.ru',
+  avito: 'Avito',
+  classified: 'Дром',
+  broker: 'СберАвто',
+  DC: 'ДЦ',
+  SBOL: 'SBOL',
+  ECREDIT: 'eCredit',
+  AST: 'АСТ',
+}
+
 export const makeApplicationTableData = (data: Application[]): PreparedTableData[] =>
   data.map(application => {
     const {
@@ -22,7 +42,7 @@ export const makeApplicationTableData = (data: Application[]): PreparedTableData
       applicationUpdateDate: applicationUpdateDate ?? '',
       fullName: getFullName(firstName, lastName, middleName),
       vendorCode: vendorCode ?? '',
-      source: source ?? '',
+      source: sourceMap[(source || '') as keyof typeof sourceMap] || source || '',
       decisionTerm: typeof decisionTerm === 'number' && decisionTerm >= 0 ? decisionTerm : '-',
       //NOTE: непонятно что отвечает за иконку уточнить
       isDC: true,
