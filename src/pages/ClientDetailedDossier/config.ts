5fe2aec69596ad56d37355c13b4a8157057245bf
Дмитрий Лысков <dmitrijlyskov@192.168.1.3>
2023-11-15 17:33:34 +0300

    DCB-890 | Подключен документ Анкета

--- src/pages/ClientDetailedDossier/config.ts
+++ src/pages/ClientDetailedDossier/config.ts
@@ -4,6 +4,8 @@ export const progressBarConfig = {
   steps: ['Сформировать договор', 'Скачать КД', 'Подписать КД', 'Проверка реквизитов'],
 }
 
-// TODO DCB-890 Тут должен быть еще DocumentType.STATEMENT_FORM, но пока Бэк не сделал его формирование,
-// и он не приходит. Добавить по готовности Бэка
-export const agreementDocTypes = [DocumentType.CREDIT_CONTRACT, DocumentType.ACCOUNT_OPEN_FORM]
+export const agreementDocTypes = [
+  DocumentType.CREDIT_CONTRACT,
+  DocumentType.ACCOUNT_OPEN_FORM,
+  DocumentType.STATEMENT_FORM,
+]
