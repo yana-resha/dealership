6fd22e14592da7f07514c72e851e5bc8a6264448
Дмитрий Лысков <dmitrijlyskov@Admins-MacBook-Pro.local>
2023-12-14 17:42:06 +0300

    DCB-1250 | Скорректирован маппинг кодов регионов

--- src/pages/CreateOrder/ClientForm/utils/prepareAddress.ts
+++ src/pages/CreateOrder/ClientForm/utils/prepareAddress.ts
@@ -1,33 +1,51 @@
 import { DataGetAddressSuggestions } from '@sberauto/dadata-proto/public'
 import { AddressFrontdc } from '@sberauto/loanapplifecycledc-proto/public'
 
-import { AREA_TYPES, CITY_TYPES, SETTLEMENT_TYPES, STREET_TYPES } from '../config/address.config'
+import {
+  AREA_TYPES,
+  CITY_TYPES,
+  DEFAULT_AREA_TYPE,
+  DEFAULT_CITY_TYPE,
+  DEFAULT_SETTLEMENT_TYPE,
+  DEFAULT_STREET_TYPE,
+  SETTLEMENT_TYPES,
+  STREET_TYPES,
+} from '../config/address.config'
 
-function getAreaType(areaTypeFull: string | undefined): string {
-  return (
-    AREA_TYPES.find(areaType => areaType.label.toLowerCase() === areaTypeFull?.toLowerCase())?.value ?? ''
-  )
+function getAreaType(areaTypeFull: string | undefined, area: string | undefined): string {
+  // На Бэке есть валидация: if area==""&& areaType !="" {err}
+  // Потому нам не нужно, что бы  при пустом area нашелся дефолтный areaType
+  return area
+    ? AREA_TYPES.find(areaType => areaType.label.toLowerCase() === areaTypeFull?.toLowerCase())?.value ??
+        DEFAULT_AREA_TYPE.value
+    : ''
 }
 
-function getCityType(cityTypeFull: string | undefined): string {
-  return (
-    CITY_TYPES.find(cityType => cityType.label.toLowerCase() === cityTypeFull?.toLowerCase())?.value ?? ''
-  )
+function getCityType(cityTypeFull: string | undefined, city: string | undefined): string {
+  // На Бэке есть валидация: if city==""&& cityType !="" {err}
+  // Потому нам не нужно, что бы  при пустом city нашелся дефолтный cityType
+  return city
+    ? CITY_TYPES.find(cityType => cityType.label.toLowerCase() === cityTypeFull?.toLowerCase())?.value ??
+        DEFAULT_CITY_TYPE.value
+    : ''
 }
 
-function getSettlementType(settlementTypeFull: string | undefined): string {
-  return (
-    SETTLEMENT_TYPES.find(
-      settlementType => settlementType.label.toLowerCase() === settlementTypeFull?.toLowerCase(),
-    )?.value ?? ''
-  )
+function getSettlementType(settlementTypeFull: string | undefined, settlement: string | undefined): string {
+  // На Бэке нет подобной валидации, но у нас будет для единообразия
+  return settlement
+    ? SETTLEMENT_TYPES.find(
+        settlementType => settlementType.label.toLowerCase() === settlementTypeFull?.toLowerCase(),
+      )?.value ?? DEFAULT_SETTLEMENT_TYPE.value
+    : ''
 }
 
-function getStreetTypes(streetTypeFull: string | undefined): string {
-  return (
-    STREET_TYPES.find(streetType => streetType.label.toLowerCase() === streetTypeFull?.toLowerCase())
-      ?.value ?? ''
-  )
+function getStreetTypes(streetTypeFull: string | undefined, street: string | undefined): string {
+  // На Бэке есть валидация: if street==""&& streetType !="" {err}
+  // Потому нам не нужно, что бы  при пустом street нашелся дефолтный streetType
+  return street
+    ? STREET_TYPES.find(streetType => streetType.label.toLowerCase() === streetTypeFull?.toLowerCase())
+        ?.value ?? DEFAULT_STREET_TYPE.value
+    : ''
 }
 
 export function prepareAddress(
@@ -37,16 +55,16 @@ export function prepareAddress(
     postalCode: addressObjectData?.postalCode ?? '',
     regCode: addressObjectData?.regionKladrId ? addressObjectData.regionKladrId.slice(0, 2) : '',
     area: addressObjectData?.area ?? '',
-    areaType: getAreaType(addressObjectData?.areaTypeFull),
+    areaType: getAreaType(addressObjectData?.areaTypeFull, addressObjectData?.area),
     city: addressObjectData?.city ?? '',
-    cityType: getCityType(addressObjectData?.cityTypeFull),
+    cityType: getCityType(addressObjectData?.cityTypeFull, addressObjectData?.city),
     house: addressObjectData?.house ?? '',
     houseExt: addressObjectData?.block ?? '',
     region: addressObjectData?.region ?? '',
     settlement: addressObjectData?.settlement ?? '',
-    settlementType: getSettlementType(addressObjectData?.settlementTypeFull),
+    settlementType: getSettlementType(addressObjectData?.settlementTypeFull, addressObjectData?.settlement),
     street: addressObjectData?.street ?? '',
-    streetType: getStreetTypes(addressObjectData?.streetTypeFull),
+    streetType: getStreetTypes(addressObjectData?.streetTypeFull, addressObjectData?.street),
     unitNum: addressObjectData?.flat ?? '',
   }
 
